bbb2752f20842d8109b46cd45cb29437b43d8575
meggranit <s1265607@monmouth.edu>
2022-11-22 11:30:40 -0500

    Initial Commit

---
+++ Suitcase/SuitcaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuitcaseApp.swift
+//  Suitcase
+//
+//  Created by meg on 11/22/22.
+//
+
+import SwiftUI
+
+@main
+struct SuitcaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
